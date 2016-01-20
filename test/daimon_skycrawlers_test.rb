e7539d0ede9f28a72915532f5c3533813b9f089e
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-01-20 13:10:11 +0900

    Use test-unit instead of minitest

--- test/daimon_skycrawlers_test.rb
+++ test/daimon_skycrawlers_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class DaimonSkycrawlersTest < Minitest::Test
+class DaimonSkycrawlersTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::DaimonSkycrawlers::VERSION
   end
