56c944e514c7097d334a7793493f2ff7845ef0e0
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-01-20 13:12:12 +0900

    Remove unnecessary test

--- test/daimon_skycrawlers_test.rb
+++ test/daimon_skycrawlers_test.rb
@@ -4,8 +4,4 @@ class DaimonSkycrawlersTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::DaimonSkycrawlers::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
