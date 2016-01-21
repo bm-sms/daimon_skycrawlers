561a8881a3b97d8a7960c8c31c4b7e3acda0b8cf
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-01-21 18:07:15 +0900

    Add generate command

---
+++ lib/daimon_skycrawlers/generator/templates/new/crawler.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require "daimon_skycrawlers/crawler"
+
+DaimonSkycrawlers::Crawler.run
---
+++ lib/daimon_skycrawlers/generator/templates/new/processor.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require "daimon_skycrawlers/processor"
+
+DaimonSkycrawlers::Processor.run
