ee0f034b52f17634286fdf47114f283670b9fdd7
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-01-26 12:12:00 +0900

    Change cwd to load `config/database.yml`

--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -6,12 +6,12 @@ require 'timeout'
 Given /^I have the "([^"]*)" application$/ do |path|
   @current_app_path = fixture_path(path)
 
-  Bundler.with_clean_env {
+  Bundler.with_clean_env do
     Dir.chdir(@current_app_path) do
       `rm -rf db/*.sqlite3`
       `bundle exec rake db:migrate`
     end
-  }
+  end
 end
 
 When /^I run crawler & processor$/ do
@@ -20,10 +20,12 @@ When /^I run crawler & processor$/ do
   @processor_log_path = "#{dir}/processor.log"
   @crawler_log_path = "#{dir}/crawler.log"
 
-  Bundler.with_clean_env {
-    @worker_pids << spawn("bundle exec ruby #{@current_app_path.join('crawler.rb')}", out: @crawler_log_path, err: @crawler_log_path)
-    @worker_pids << spawn("bundle exec ruby #{@current_app_path.join('processor.rb')}", out: @processor_log_path, err: @processor_log_path)
-  }
+  Bundler.with_clean_env do
+    Dir.chdir(@current_app_path) do
+      @worker_pids << spawn("bundle exec ruby #{@current_app_path.join('processor.rb')}", out: @processor_log_path, err: @processor_log_path)
+      @worker_pids << spawn("bundle exec ruby #{@current_app_path.join('crawler.rb')}", out: @crawler_log_path, err: @crawler_log_path)
+    end
+  end
 end
 
 Then /^processor receives the following message:$/ do |message|
