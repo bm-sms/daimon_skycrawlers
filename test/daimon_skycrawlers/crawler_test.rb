bbad56d75327e5e77c896866f77388c5bc9607ed
Kenji Okimoto <okimoto@clear-code.com>
2016-02-02 12:02:54 +0900

    Mock processor queue

--- test/daimon_skycrawlers/crawler_test.rb
+++ test/daimon_skycrawlers/crawler_test.rb
@@ -37,6 +37,7 @@ class DaimonSkycrawlersCrawlerTest < Test::Unit::TestCase
         faraday.adapter :test, stubs
       end
       @crawler.storage = DaimonSkycrawlers::Storage::Null.new
+      mock(@crawler).schedule_to_process("http://example.com/blog")
     end
 
     def test_fetch_blog
