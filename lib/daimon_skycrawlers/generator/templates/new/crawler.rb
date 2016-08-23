d6321d5da6d80f4a12d45551fa378cedb5c02041
Kenji Okimoto <okimoto@clear-code.com>
2016-08-23 16:48:13 +0900

    Remove parser related code

--- lib/daimon_skycrawlers/generator/templates/new/crawler.rb
+++ lib/daimon_skycrawlers/generator/templates/new/crawler.rb
@@ -5,9 +5,6 @@ require "daimon_skycrawlers/crawler"
 base_url = "http://example.com"
 
 crawler = DaimonSkycrawlers::Crawler.new(base_url)
-crawler.parser.append_filter do |url|
-  url.start_with?(base_url)
-end
 
 DaimonSkycrawlers.register_crawler(crawler)
 
