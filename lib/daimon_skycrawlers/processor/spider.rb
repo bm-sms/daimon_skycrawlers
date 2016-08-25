21539c5d3966fabe28a69983f8bff00ecd29f0d5
Kenji Okimoto <okimoto@clear-code.com>
2016-08-25 13:05:58 +0900

    Add simple spider

---
+++ lib/daimon_skycrawlers/processor/spider.rb
@@ -0,0 +1,53 @@
+module DaimonSkycrawlers
+  class Processor
+    class Spider < Base
+      def initialize
+        @filters = []
+        @doc = nil
+        @links = nil
+      end
+
+      def append_fileter(filter = nil, &block)
+        if block_given?
+          @filters << block
+        else
+          @filters << filter
+        end
+      end
+
+      def call(message)
+        page = storage.find(message[:url])
+        @doc = Nokogiri::HTML(page.body)
+        links.each do |url|
+          DaimonSkycrawlers::Crawler.enqueue_url(url)
+        end
+      end
+
+      def links
+        return @links if @links
+        @links = retrieve_links
+        @links
+      end
+
+      private
+
+      def retrieve_links
+        urls = @doc.search("a").map do |element|
+          element["href"]
+        end
+        apply_filters(urls) || []
+      end
+
+      def apply_filters(urls)
+        return if urls.nil?
+        return if urls.empty?
+        @filters.each do |filter|
+          urls = urls.select do |url|
+            filter.call(url)
+          end
+        end
+        urls
+      end
+    end
+  end
+end
