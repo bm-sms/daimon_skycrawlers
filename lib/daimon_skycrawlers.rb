8a15959105574a5c654f94d5bc7861fed2e75435
Kenji Okimoto <okimoto@clear-code.com>
2016-08-30 13:29:18 +0900

    Add DaimonSkycrawlers::Configuration

--- lib/daimon_skycrawlers.rb
+++ lib/daimon_skycrawlers.rb
@@ -1,8 +1,13 @@
 require "bundler/setup"
 
 require "daimon_skycrawlers/version"
+require "daimon_skycrawlers/logger"
 
 module DaimonSkycrawlers
+  Configuration = Struct.new(
+    :logger,
+    :crawler_interval
+  )
   class << self
     def register_processor(processor = nil, &block)
       DaimonSkycrawlers::Consumer::HTTPResponse.register(processor, &block)
@@ -10,6 +15,17 @@ module DaimonSkycrawlers
 
     def register_crawler(crawler)
       DaimonSkycrawlers::Consumer::URL.register(crawler)
+    end
+
+    def configuration
+      @configuration ||= DaimonSkycrawlers::Configuration.new.tap do |config|
+        config.logger = DaimonSkycrawlers::Logger.default
+        config.crawler_interval = 1
+      end
+    end
+
+    def configure
+      yield configuration
     end
   end
 end
