78eca7b2f975985f50a267faf2f8aa8ff4c8b3ed
Kenji Okimoto <okimoto@clear-code.com>
2016-08-30 13:22:10 +0900

    Simplify code

--- lib/daimon_skycrawlers/queue.rb
+++ lib/daimon_skycrawlers/queue.rb
@@ -3,21 +3,22 @@ require "songkick_queue"
 module DaimonSkycrawlers
   class Queue
     class << self
+      def configuration
+        @configuration ||= SongkickQueue.configure do |config|
+          config.logger = Logger.new(STDOUT)
+          config.host = "127.0.0.1"
+          config.port = 5672
+          # config.username = 'guest'
+          # config.password = 'guest'
+          config.vhost = "/"
+          config.max_reconnect_attempts = 10
+          config.network_recovery_interval = 1.0
+        end
+      end
+
       def configure
-        yield SongkickQueue.configuration
+        yield configuration
       end
     end
   end
-end
-
-# Default configuration
-SongkickQueue.configure do |config|
-  config.logger = Logger.new(STDOUT)
-  config.host = "127.0.0.1"
-  config.port = 5672
-  # config.username = 'guest'
-  # config.password = 'guest'
-  config.vhost = "/"
-  config.max_reconnect_attempts = 10
-  config.network_recovery_interval = 1.0
 end
