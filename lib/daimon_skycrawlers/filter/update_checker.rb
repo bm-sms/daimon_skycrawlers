401435f0ad2d370bd2141ccc22006d8620e6ca0e
Kenji Okimoto <okimoto@clear-code.com>
2016-09-06 15:33:48 +0900

    Use specified connection to check update

--- lib/daimon_skycrawlers/filter/update_checker.rb
+++ lib/daimon_skycrawlers/filter/update_checker.rb
@@ -4,10 +4,14 @@ require "daimon_skycrawlers/filter/base"
 module DaimonSkycrawlers
   module Filter
     class UpdateChecker < Base
-      def call(url)
+      def call(url, connection: nil)
         page = storage.find(url)
         return true unless page
-        headers = Faraday.head(url)
+        if connection
+          headers = connection.head(url)
+        else
+          headers = Faraday.head(url)
+        end
         return false if headers["etag"] && page.etag && headers["etag"] == page.etag
         return false if headers["last-modified"].nil? && page.last_modified_at.nil?
         return false if headers["last-modified"] <= page.last_modified_at
