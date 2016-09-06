74b8100b3e3239854b2a44537a00acaabf43b460
Kenji Okimoto <okimoto@clear-code.com>
2016-09-06 15:05:41 +0900

    Add missing require

--- lib/daimon_skycrawlers/generator/templates/new/enqueue.rb
+++ lib/daimon_skycrawlers/generator/templates/new/enqueue.rb
@@ -5,6 +5,8 @@ require "thor"
 require "daimon_skycrawlers/crawler"
 require "daimon_skycrawlers/processor"
 
+require_relative "./init"
+
 class Enqueue < Thor
   desc "url URL [key1:value1 key2:value2...]", "Enqueue URL for URL consumer"
   def url(url, *rest)
