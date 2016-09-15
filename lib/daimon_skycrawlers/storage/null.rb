fcaee055240b3694bc86f5e8369d70d2e9487487
Kenji Okimoto <okimoto@clear-code.com>
2016-09-15 15:20:54 +0900

    Add YARD style comment

--- lib/daimon_skycrawlers/storage/null.rb
+++ lib/daimon_skycrawlers/storage/null.rb
@@ -1,9 +1,21 @@
 module DaimonSkycrawlers
   module Storage
+    #
+    # The null storage.
+    #
+    # This storage is useful for test.
+    #
     class Null < Base
+
+      #
+      # Save nothing
+      #
       def save(url, headers, body)
       end
 
+      #
+      # Find nothing
+      #
       def find(url)
       end
     end
--- lib/daimon_skycrawlers/storage/rdb.rb
+++ lib/daimon_skycrawlers/storage/rdb.rb
@@ -3,13 +3,24 @@ require "active_record"
 
 module DaimonSkycrawlers
   module Storage
+    #
+    # Storage for RDBMS
+    #
     class RDB < Base
       def initialize(config_path = "config/database.yml")
+        super()
         config = YAML.load_file(config_path)
         environment = ENV["SKYCRAWLERS_ENV"] || "development"
         ActiveRecord::Base.establish_connection(config[environment])
       end
 
+      #
+      # Save
+      #
+      # @param [String] url identity of the page
+      # @param [Hash] header of URL
+      # @param [String] body
+      #
       def save(url, headers, body)
         Page.create(url: url,
                     headers: JSON.generate(headers),
@@ -18,6 +29,11 @@ module DaimonSkycrawlers
                     etag: headers["etag"])
       end
 
+      #
+      # Fetch page identified by url
+      #
+      # @param [String] url identity of the page
+      #
       def find(url)
         Page.where(url: url).order(last_modified_at: :desc).limit(1).first
       end
