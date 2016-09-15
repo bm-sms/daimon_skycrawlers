2c8fc80fe46465e057c8718eb1ab671fba92e43e
Kenji Okimoto <okimoto@clear-code.com>
2016-09-15 12:01:55 +0900

    Add YARD style document partially

--- daimon_skycrawlers.gemspec
+++ daimon_skycrawlers.gemspec
@@ -35,4 +35,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "pry"
   spec.add_development_dependency "tapp"
   spec.add_development_dependency "sqlite3"
+  spec.add_development_dependency "yard"
 end
--- lib/daimon_skycrawlers/crawler.rb
+++ lib/daimon_skycrawlers/crawler.rb
@@ -6,24 +6,50 @@ require "daimon_skycrawlers/consumer/url"
 module DaimonSkycrawlers
   module Crawler
     class << self
+      #
+      # Run registered crawlers
+      #
+      # @param process_name [String] Process name
+      #
       def run(process_name: default_process_name)
         DaimonSkycrawlers::Timer.setup_shutdown_timer(config.queue_name_prefix, interval: config.shutdown_interval)
         SongkickQueue::Worker.new(process_name, [DaimonSkycrawlers::Consumer::URL]).run
       end
 
+      #
+      # Enqueue a URL to crawler queue
+      #
+      # @param [String] Specify URL
+      # @param [Hash] Extra parameters for crawler
+      # @return [void]
       def enqueue_url(url, message = {})
         message[:url] = url
         SongkickQueue.publish(queue_name, message)
       end
 
+      #
+      # Shortcut of DaimonSkycrawlers.configuration
+      #
+      # @return [DaimonSkycrawlers::Configuration]
+      #
       def config
         DaimonSkycrawlers.configuration
       end
 
+      #
+      # Queue name for crawler
+      #
+      # @return [String] Queue name
+      #
       def queue_name
         "#{config.queue_name_prefix}.url"
       end
 
+      #
+      # Default process name
+      #
+      # @return [String] Default process name
+      #
       def default_process_name
         "#{config.queue_name_prefix}:url"
       end
