4b63d9ba183b44faf82a4b373d7e3aeef5f53a91
Kenji Okimoto <okimoto@clear-code.com>
2016-09-20 15:38:30 +0900

    Generate migration files using MigrationGenerator
    In previous version, we use ActiveRecord 4.x.
    In this version, we use ActiveRecord 4.x or 5.x according to installed
    ActiveRecord version.

--- lib/daimon_skycrawlers/generator/new.rb
+++ lib/daimon_skycrawlers/generator/new.rb
@@ -1,4 +1,8 @@
 require "thor"
+require "rails/generators"
+require "rails/generators/actions"
+require "rails/generators/active_record"
+require "rails/generators/active_record/migration/migration_generator"
 
 module DaimonSkycrawlers
   module Generator
@@ -18,6 +22,16 @@ module DaimonSkycrawlers
         ].each do |path|
           template("#{path}.erb", "#{name}/#{path}")
         end
+        invoke(MigrationGenerator, [
+                 "CreatePage",
+                 "url:string",
+                 "headers:text",
+                 "body:binary",
+                 "last_modified_at:datetime",
+                 "etag:string",
+                 "timestamps"
+               ],
+          { destination_root: File.join(destination_root, name) })
       end
 
       def copy_files
@@ -31,12 +45,19 @@ module DaimonSkycrawlers
         ].each do |path|
           copy_file(path, "#{name}/#{path}")
         end
-        [
-          "db/migrate/create_pages.rb",
-        ].each do |path|
-          migration = "#{Time.now.strftime("%Y%m%d%H%M%S")}_#{File.basename(path)}"
-          copy_file(path, "#{name}/db/migrate/#{migration}")
-        end
+      end
+    end
+
+    class MigrationGenerator < ActiveRecord::Generators::MigrationGenerator
+      def self.source_root
+        ActiveRecord::Generators::MigrationGenerator.source_root
+      end
+
+      def create_migration_file
+        set_local_assigns!
+        validate_file_name!
+        dest = options[:destination_root]
+        migration_template @migration_template, "#{dest}/db/migrate/#{file_name}.rb"
       end
     end
   end
