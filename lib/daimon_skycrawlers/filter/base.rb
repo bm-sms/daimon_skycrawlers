adaff5b04620c548cd7f4894e4fc79461e645f40
Kenji Okimoto <okimoto@clear-code.com>
2016-10-04 16:47:50 +0900

    Add comment to Filter::Base

--- lib/daimon_skycrawlers/filter/base.rb
+++ lib/daimon_skycrawlers/filter/base.rb
@@ -3,6 +3,14 @@ require "daimon_skycrawlers/config"
 
 module DaimonSkycrawlers
   module Filter
+    #
+    # Base class of filters.
+    #
+    # You must implement `#call` in your filter and it must return
+    # true or false. If your filter returns true, processors can
+    # process given URL after your filter. Otherwise framework skips
+    # given URL to skip processors.
+    #
     class Base
       include DaimonSkycrawlers::LoggerMixin
       include DaimonSkycrawlers::ConfigMixin
