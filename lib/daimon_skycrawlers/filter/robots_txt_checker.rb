1467ee8272e685375ae200595253319bd98698fa
Kenji Okimoto <okimoto@clear-code.com>
2016-10-04 14:49:44 +0900

    Add document

--- lib/daimon_skycrawlers/filter/robots_txt_checker.rb
+++ lib/daimon_skycrawlers/filter/robots_txt_checker.rb
@@ -4,12 +4,20 @@ require "daimon_skycrawlers/version"
 
 module DaimonSkycrawlers
   module Filter
+    #
+    # This filter provides robots.txt checker for given URL.
+    # We want to obey robots.txt provided by a web site.
+    #
     class RobotsTxtChecker < Base
       def initialize(base_url: nil, user_agent: "DaimonSkycrawlers/#{DaimonSkycrawlers::VERSION}")
         super
         @webrobots = WebRobots.new(user_agent)
       end
 
+      #
+      # @param [String] url
+      # @return [true|false] Return true when web site allows to fetch the URL, otherwise return false
+      #
       def call(url)
         unless URI(url).absolute?
           url = (@base_url + url).to_s
