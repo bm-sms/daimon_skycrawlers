6ba743d017a4516cb5ed0c8263044208f1b66a7b
Kenji Okimoto <okimoto@clear-code.com>
2016-10-11 12:41:23 +0900

    test: Follow previous change

--- test/daimon_skycrawlers/crawler_test.rb
+++ test/daimon_skycrawlers/crawler_test.rb
@@ -57,7 +57,7 @@ class DaimonSkycrawlersCrawlerTest < Test::Unit::TestCase
   sub_test_case "filter" do
     def test_robotx_txt
       crawler = ::DaimonSkycrawlers::Crawler::Default.new("http://example.com", options: { obey_robots_txt: true })
-      robots_txt_checker = mock(Object.new).call(anything) { false }
+      robots_txt_checker = mock(Object.new).allowed?(anything) { false }
       mock(DaimonSkycrawlers::Filter::RobotsTxtChecker).new(anything) { robots_txt_checker }
       crawler.storage = DaimonSkycrawlers::Storage::Null.new
       mock(crawler).schedule_to_process("http://example.com/blog", { heartbeat: true })
@@ -70,7 +70,7 @@ class DaimonSkycrawlersCrawlerTest < Test::Unit::TestCase
 
     def test_update_checker
       crawler = ::DaimonSkycrawlers::Crawler::Default.new("http://example.com", options: { obey_robots_txt: true })
-      update_checker = mock(Object.new).call(anything, anything) { false }
+      update_checker = mock(Object.new).updated?(anything, anything) { false }
       mock(DaimonSkycrawlers::Filter::UpdateChecker).new(anything) { update_checker }
       crawler.storage = DaimonSkycrawlers::Storage::Null.new
       mock(crawler).schedule_to_process("http://example.com/blog", { heartbeat: true })
