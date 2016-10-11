1d1a9c575aca2f1d6622c8777df24d33c4c78a02
Kenji Okimoto <okimoto@clear-code.com>
2016-10-11 16:21:48 +0900

    processor: Add hook to skip processing before invoke call

--- lib/daimon_skycrawlers/processor/base.rb
+++ lib/daimon_skycrawlers/processor/base.rb
@@ -7,7 +7,21 @@ module DaimonSkycrawlers
       include DaimonSkycrawlers::LoggerMixin
       include DaimonSkycrawlers::ConfigMixin
 
+      def initialize
+        super
+        @before_process_filters = []
+      end
+
+      def before_process(filter = nil, &block)
+        if block_given?
+          @before_process_filters << block
+        else
+          @before_process_filters << filter if filter.respond_to?(:call)
+        end
+      end
+
       def process(message)
+        return unless apply_before_filters(message[:url])
         call(message)
       end
 
@@ -17,6 +31,12 @@ module DaimonSkycrawlers
 
       def storage
         @storage ||= DaimonSkycrawlers::Storage::RDB.new
+      end
+
+      def apply_before_filters(url)
+        @before_process_filters.all? do |filter|
+          filter.call(url)
+        end
       end
     end
   end
