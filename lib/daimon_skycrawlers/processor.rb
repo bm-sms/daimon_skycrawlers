43178647f9005cbccefd6f18402158ebc33cc991
Kenji Okimoto <okimoto@clear-code.com>
2016-10-25 12:59:44 +0900

    Add comment

--- lib/daimon_skycrawlers/processor.rb
+++ lib/daimon_skycrawlers/processor.rb
@@ -6,25 +6,51 @@ require "daimon_skycrawlers/consumer/http_response"
 module DaimonSkycrawlers
   module Processor
     class << self
+      #
+      # Run registered processors
+      #
+      # @param process_name [String] Process name
+      #
       def run(process_name: default_process_name)
         DaimonSkycrawlers::Timer.setup_shutdown_timer(config.queue_name_prefix, interval: config.shutdown_interval)
         SongkickQueue::Worker.new(process_name, [DaimonSkycrawlers::Consumer::HTTPResponse]).run
       end
 
+      #
+      # Enqueue a URL to processor queue
+      #
+      # @param [String] Specify absolute URL
+      # @param [Hash] Extra parameters for crawler
+      # @return [void]
       def enqueue_http_response(url, message = {})
         message[:url] = url
         config.logger.debug("#{queue_name}: #{url}")
         SongkickQueue.publish(queue_name, message)
       end
 
+      #
+      # Shortcut of DaimonSkycrawlers.configuration
+      #
+      # @return [DaimonSkycrawlers::Configuration]
+      #
       def config
         DaimonSkycrawlers.configuration
       end
 
+      #
+      # Queue name for processor
+      #
+      # @return [String] Queue name
+      #
       def queue_name
         "#{config.queue_name_prefix}.http-response"
       end
 
+      #
+      # Default process name
+      #
+      # @return [String] Default process name
+      #
       def default_process_name
         "#{config.queue_name_prefix}:http-response"
       end
