cf2d0dea2afaa3fb78be0028b2a04c39b680d025
Kenji Okimoto <okimoto@clear-code.com>
2016-11-17 16:57:27 +0900

    Support RabbitMQ related environment variables

--- lib/daimon_skycrawlers/generator/templates/new/config/init.rb
+++ lib/daimon_skycrawlers/generator/templates/new/config/init.rb
@@ -6,16 +6,22 @@ require "daimon_skycrawlers/queue"
 DaimonSkycrawlers.configure do |config|
   config.logger = DaimonSkycrawlers::Logger.default
   config.crawler_interval = 1
+  config.shutdown_interval = 300
 end
 
 DaimonSkycrawlers::Queue.configure do |config|
-  #  queue configuration
+  if ENV["CLOUDAMQP_URL"]
+    amqp_uri = URI(ENV["CLOUDAMQP_URL"])
+    config.host = amqp_uri.host
+    config.username = amqp_uri.user
+    config.password = amqp_uri.password
+    config.vhost = amqp_uri.user
+  else
+    config.port = 5672
+    config.host = ENV["SKYCRAWLERS_RABBITMQ_HOST"] || "localhost"
+    config.vhost = "/"
+  end
   config.logger = DaimonSkycrawlers.configuration.logger
-  config.host = "127.0.0.1"
-  config.port = 5672
-  # config.username = 'guest'
-  # config.password = 'guest'
-  config.vhost = "/"
   config.max_reconnect_attempts = 10
   config.network_recovery_interval = 1.0
 end
