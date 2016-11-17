54ea3401c68083bcbbe409267e8fd574a31c4761
Kenji Okimoto <okimoto@clear-code.com>
2016-11-17 17:00:52 +0900

    Organize boot up sequence

--- lib/daimon_skycrawlers/generator/templates/new/services/common/docker-entrypoint.sh
+++ lib/daimon_skycrawlers/generator/templates/new/services/common/docker-entrypoint.sh
@@ -4,13 +4,22 @@ set -x
 
 MAIN=$1
 case $MAIN in
-    crawler|processor)
-        bundle check || bundle install --retry=3 --path=vendor/bundle && bundle exec rake db:setup
+    crawler)
+        bundle check || bundle install --retry=3 --path=vendor/bundle \
+                && bundle exec rake db:schema:load || bundle exec rake db:migrate
+        bundle exec daimon_skycrawlers exec $MAIN
+        ;;
+    processor)
+        while [ ! -e Gemfile.lock ]; do
+            sleep 5
+        done
+        bundle check || bundle install --retry=3 --path=vendor/bundle \
+                && bundle exec rake db:schema:load || bundle exec rake db:migrate
         bundle exec daimon_skycrawlers exec $MAIN
         ;;
     setup)
         bundle install --path=vendor/bundle
-        bundle exec rake db:setup
+        bundle exec rake db:schema:load
         ;;
     none)
         echo NOP
