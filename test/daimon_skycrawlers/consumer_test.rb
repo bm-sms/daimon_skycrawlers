85267d254afd66e45105ee13c5b4b4256ad9ab33
Kenji Okimoto <okimoto@clear-code.com>
2016-11-17 09:56:02 +0900

    Add test for HTTP response consumer

---
+++ test/daimon_skycrawlers/consumer_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class DaimonSkycrawlersConsumerTest < Test::Unit::TestCase
+  class DummyProcessor < DaimonSkycrawlers::Processor::Base
+    attr_reader :called
+
+    def initialize
+      super
+      @called = false
+    end
+
+    def call(message)
+      @called = true
+    end
+  end
+
+  sub_test_case "HTTPResponse" do
+    test "default processor" do
+      message = { url: "http://example.com/", heartbeat: true }
+      consumer = DaimonSkycrawlers::Consumer::HTTPResponse.new("test", ::Logger.new(nil))
+      assert_nothing_raised do
+        consumer.process(message)
+      end
+    end
+
+    test "block processor" do
+      message = { url: "http://example.com/" }
+      called = false
+      DaimonSkycrawlers::Consumer::HTTPResponse.register do |_message|
+        called = true
+      end
+      consumer = DaimonSkycrawlers::Consumer::HTTPResponse.new("test", ::Logger.new(nil))
+      assert_false(called)
+      consumer.process(message)
+      assert_true(called)
+    end
+
+    test "custom processor" do
+      message = { url: "http://example.com/" }
+      dummy_processor = DummyProcessor.new
+      DaimonSkycrawlers::Consumer::HTTPResponse.register(dummy_processor)
+      consumer = DaimonSkycrawlers::Consumer::HTTPResponse.new("test", ::Logger.new(nil))
+      assert_false(dummy_processor.called)
+      consumer.process(message)
+      assert_true(dummy_processor.called)
+    end
+  end
+end
