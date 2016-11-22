ea47178e63dce32510cce1712fbf656b69d6494b
Kenji Okimoto <okimoto@clear-code.com>
2016-11-22 09:50:05 +0900

    template: Describe return value

--- lib/daimon_skycrawlers/generator/templates/new/app/filters/sample_filter.rb
+++ lib/daimon_skycrawlers/generator/templates/new/app/filters/sample_filter.rb
@@ -3,5 +3,6 @@ require "daimon_skycrawlers/filter/base"
 class SampleFilter < DaimonSkycrawlers::Filter::Base
   def call(url)
     # Imprement your filter here.
+    # If you want to crawl `url`, return true otherwise false.
   end
 end
