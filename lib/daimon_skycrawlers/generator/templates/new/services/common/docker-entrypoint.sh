0a2076bb4a91b4dc02ed4933a0b729539a8a6221
Kenji Okimoto <okimoto@clear-code.com>
2016-12-06 11:25:33 +0900

    Wait for services always

--- lib/daimon_skycrawlers/generator/templates/new/services/common/docker-entrypoint.sh
+++ lib/daimon_skycrawlers/generator/templates/new/services/common/docker-entrypoint.sh
@@ -3,19 +3,16 @@
 set -x
 
 MAIN=$1
+dockerize -timeout 60s \
+          -wait tcp://${DATABASE_HOST}:${DATABASE_PORT} \
+          -wait tcp://${SKYCRAWLERS_RABBITMQ_HOST}:${SKYCRAWLERS_RABBITMQ_PORT}
 case $MAIN in
     crawler)
-        dockerize -timeout 60s \
-                  -wait tcp://${DATABASE_HOST}:${DATABASE_PORT} \
-                  -wait tcp://${SKYCRAWLERS_RABBITMQ_HOST}:${SKYCRAWLERS_RABBITMQ_PORT}
         bundle check || bundle install --retry=3 --path=vendor/bundle \
                 && bundle exec rake db:migrate
         bundle exec daimon_skycrawlers exec $MAIN
         ;;
     processor)
-        dockerize -timeout 60s \
-                  -wait tcp://${DATABASE_HOST}:${DATABASE_PORT} \
-                  -wait tcp://${SKYCRAWLERS_RABBITMQ_HOST}:${SKYCRAWLERS_RABBITMQ_PORT}
         while [ ! -e Gemfile.lock ]; do
             sleep 5
         done
