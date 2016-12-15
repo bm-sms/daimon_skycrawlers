481dbb5df48383cddbe97d03f71a638c7272feac
Kenji Okimoto <okimoto@clear-code.com>
2016-12-15 11:23:03 +0900

    Display post message when generate crawler/processor

--- lib/daimon_skycrawlers/generator/crawler.rb
+++ lib/daimon_skycrawlers/generator/crawler.rb
@@ -17,6 +17,19 @@ module DaimonSkycrawlers
         }
         template("crawler.rb.erb", "app/crawlers/#{name.underscore}.rb", config)
       end
+
+      def display_post_message
+        puts <<MESSAGE
+
+You can register your crawler in `app/crawler.rb` to run your crawler.
+Following code snippet is useful:
+
+    base_url = "https://www.example.com/"
+    crawler = #{name.classify}.new(base_url)
+    DaimonSkycrawlers.register_crawler(crawler)
+
+MESSAGE
+      end
     end
   end
 end
