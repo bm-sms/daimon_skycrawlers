0fa0c4570189d21df80578e35e823758f5696f2c
Kenji Okimoto <okimoto@clear-code.com>
2016-12-15 11:36:48 +0900

    Remove sample filter

--- lib/daimon_skycrawlers/generator/templates/new/app/filters/sample_filter.rb
+++
@@ -1,9 +0,0 @@
-require "daimon_skycrawlers/filter/base"
-
-class SampleFilter < DaimonSkycrawlers::Filter::Base
-  def call(message)
-    # Imprement your filter here.
-    # If you want to crawl `url`, return true otherwise false.
-    true
-  end
-end
