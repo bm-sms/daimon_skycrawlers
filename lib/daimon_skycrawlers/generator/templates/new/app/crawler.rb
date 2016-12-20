f10e415979c023710ee5979ebf4cfda251ad437c
Kenji Okimoto <okimoto@clear-code.com>
2016-12-20 12:27:30 +0900

    Use load_crawlers/load_processors

--- lib/daimon_skycrawlers/generator/templates/new/app/crawler.rb
+++ lib/daimon_skycrawlers/generator/templates/new/app/crawler.rb
@@ -1,6 +1,8 @@
 require "daimon_skycrawlers/crawler"
 require "daimon_skycrawlers/crawler/default"
 
+DaimonSkycrawlers.load_crawlers
+
 base_url = "http://example.com"
 
 crawler = DaimonSkycrawlers::Crawler::Default.new(base_url)
