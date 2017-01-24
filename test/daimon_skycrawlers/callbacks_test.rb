aaa96a72d8d67222903263db8bc81a4c71ba4559
Kenji Okimoto <okimoto@clear-code.com>
2017-01-24 12:44:30 +0900

    test: Group before callbacks related test

--- test/daimon_skycrawlers/callbacks_test.rb
+++ test/daimon_skycrawlers/callbacks_test.rb
@@ -28,98 +28,100 @@ class DaimonSkycrawlersCallbacksTest < Test::Unit::TestCase
     end
   end
 
-  test "sequence" do
-    dummy = Dummy.new
-    filter_log = []
-    dummy.before_process do |_message|
-      filter_log << 1
-      true
+  sub_test_case "before" do
+    test "sequence" do
+      dummy = Dummy.new
+      filter_log = []
+      dummy.before_process do |_message|
+        filter_log << 1
+        true
+      end
+      dummy.before_process do |_message|
+        filter_log << 2
+        true
+      end
+      dummy.before_process do |_message|
+        filter_log << 3
+        true
+      end
+      dummy.process({})
+      assert_equal([1, 2, 3], filter_log)
+      assert_true(dummy.processed)
     end
-    dummy.before_process do |_message|
-      filter_log << 2
-      true
-    end
-    dummy.before_process do |_message|
-      filter_log << 3
-      true
-    end
-    dummy.process({})
-    assert_equal([1, 2, 3], filter_log)
-    assert_true(dummy.processed)
-  end
 
-  test "sequence class" do
-    dummy = Dummy.new
-    filter1 = DummyFilter.new
-    filter2 = DummyFilter.new
-    filter3 = DummyFilter.new
-    dummy.before_process(filter1)
-    dummy.before_process(filter2)
-    dummy.before_process(filter3)
-    dummy.process({})
-    assert_true(filter1.called)
-    assert_true(filter2.called)
-    assert_true(filter3.called)
-    stamps = [filter1.stamp, filter2.stamp, filter3.stamp]
-    assert_equal(stamps, stamps.sort)
-    assert_true(dummy.processed)
-  end
+    test "sequence class" do
+      dummy = Dummy.new
+      filter1 = DummyFilter.new
+      filter2 = DummyFilter.new
+      filter3 = DummyFilter.new
+      dummy.before_process(filter1)
+      dummy.before_process(filter2)
+      dummy.before_process(filter3)
+      dummy.process({})
+      assert_true(filter1.called)
+      assert_true(filter2.called)
+      assert_true(filter3.called)
+      stamps = [filter1.stamp, filter2.stamp, filter3.stamp]
+      assert_equal(stamps, stamps.sort)
+      assert_true(dummy.processed)
+    end
 
-  test "sequence mixed" do
-    dummy = Dummy.new
-    filter = DummyFilter.new
-    stamp1 = nil
-    stamp2 = nil
-    dummy.before_process do |_message|
-      stamp1 = Time.now.to_f
-      true
+    test "sequence mixed" do
+      dummy = Dummy.new
+      filter = DummyFilter.new
+      stamp1 = nil
+      stamp2 = nil
+      dummy.before_process do |_message|
+        stamp1 = Time.now.to_f
+        true
+      end
+      dummy.before_process(filter)
+      dummy.before_process do |_message|
+        stamp2 = Time.now.to_f
+        true
+      end
+      dummy.process({})
+      stamps = [stamp1, filter.stamp, stamp2]
+      assert_equal(stamps, stamps.sort)
+      assert_true(dummy.processed)
     end
-    dummy.before_process(filter)
-    dummy.before_process do |_message|
-      stamp2 = Time.now.to_f
-      true
-    end
-    dummy.process({})
-    stamps = [stamp1, filter.stamp, stamp2]
-    assert_equal(stamps, stamps.sort)
-    assert_true(dummy.processed)
-  end
 
-  test "not process if all filter returns false" do
-    dummy = Dummy.new
-    dummy.before_process do |_message|
-      false
+    test "not process if all filter returns false" do
+      dummy = Dummy.new
+      dummy.before_process do |_message|
+        false
+      end
+      dummy.before_process do |_message|
+        false
+      end
+      dummy.process({})
+      assert_false(dummy.processed)
     end
-    dummy.before_process do |_message|
-      false
-    end
-    dummy.process({})
-    assert_false(dummy.processed)
-  end
 
-  test "not process if 1 filter returns false" do
-    dummy = Dummy.new
-    dummy.before_process do |_message|
-      true
+    test "not process if 1 filter returns false" do
+      dummy = Dummy.new
+      dummy.before_process do |_message|
+        true
+      end
+      dummy.before_process do |_message|
+        false
+      end
+      dummy.process({})
+      assert_false(dummy.processed)
     end
-    dummy.before_process do |_message|
-      false
-    end
-    dummy.process({})
-    assert_false(dummy.processed)
-  end
 
-  test "clear before process callbacks" do
-    dummy = Dummy.new
-    filter_logs = []
-    dummy.before_process do |_message|
-      filter_logs << 1
+    test "clear before process callbacks" do
+      dummy = Dummy.new
+      filter_logs = []
+      dummy.before_process do |_message|
+        filter_logs << 1
+      end
+      dummy.before_process do |_message|
+        filter_logs << 2
+      end
+      dummy.clear_before_process_callbacks
+      dummy.process({})
+      assert_true(filter_logs.empty?)
     end
-    dummy.before_process do |_message|
-      filter_logs << 2
-    end
-    dummy.clear_before_process_callbacks
-    dummy.process({})
-    assert_true(filter_logs.empty?)
   end
 end
