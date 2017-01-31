e2e5b7b96a4df6f94e88c3a907b80fbb9ed7104e
Kenji Okimoto <okimoto@clear-code.com>
2017-01-31 13:54:25 +0900

    Add test for timers

---
+++ test/daimon_skycrawlers/test_timer.rb
@@ -0,0 +1,26 @@
+require "daimon_skycrawlers/timer"
+require "helper"
+
+class TimerTest < Test::Unit::TestCase
+  test "simple timer" do
+    called = false
+    timers = DaimonSkycrawlers::Timer.setup_shutdown_timer("test", interval: 1) do
+      called = true
+    end
+    sleep(2)
+    assert(called)
+    timers.cancel
+  end
+
+  test "reset timer" do
+    called = false
+    timers = DaimonSkycrawlers::Timer.setup_shutdown_timer("test", interval: 1) do
+      called = true
+    end
+    sleep(0.5)
+    ActiveSupport::Notifications.publish("consume_message.songkick_queue", Time.now, Time.now, 1, { queue_name: "test" })
+    sleep(1)
+    assert(!called)
+    timers.cancel
+  end
+end
