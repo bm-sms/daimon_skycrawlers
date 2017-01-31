69a658b2d3fac63a38499557d17b36d42a3ba070
Kenji Okimoto <okimoto@clear-code.com>
2017-01-31 11:49:22 +0900

    Add test for multiple urls and sitemapindex

--- test/daimon_skycrawlers/test_sitemap_parser.rb
+++ test/daimon_skycrawlers/test_sitemap_parser.rb
@@ -44,4 +44,85 @@ class SitemapParserTest < Test::Unit::TestCase
     ]
     assert_equal(expected, sitemap_parser.parse)
   end
+
+  test "multiple sitemaps" do
+    urls = [
+      "https://example.com/sitemap.xml",
+      "https://example.com/sitemap1.xml",
+      "https://example.com/sitemap2.xml",
+    ]
+    responses = urls.map do |url|
+      Typhoeus::Response.new(code: 200,
+                             effective_url: url,
+                             headers: {},
+                             body: fixture_path("sitemap/#{File.basename(url)}").read)
+    end
+    urls.zip(responses) do |url, response|
+      Typhoeus.stub(url).and_return(response)
+    end
+    sitemap_parser = DaimonSkycrawlers::SitemapParser.new(urls)
+    expected = [
+      "https://example.com/1",
+      "https://example.com/2",
+      "https://example.com/11",
+      "https://example.com/12",
+      "https://example.com/21",
+      "https://example.com/22",
+    ]
+    assert_equal(expected, sitemap_parser.parse)
+  end
+
+  test "sitemapindex" do
+    index_url = "https://example.com/sitemap-index.xml"
+    urls = [
+      index_url,
+      "https://example.com/sitemap1.xml",
+      "https://example.com/sitemap2.xml",
+    ]
+    responses = urls.map do |url|
+      Typhoeus::Response.new(code: 200,
+                             effective_url: url,
+                             headers: {},
+                             body: fixture_path("sitemap/#{File.basename(url)}").read)
+    end
+    urls.zip(responses) do |url, response|
+      Typhoeus.stub(url).and_return(response)
+    end
+    sitemap_parser = DaimonSkycrawlers::SitemapParser.new([index_url])
+    expected = [
+      "https://example.com/11",
+      "https://example.com/12",
+      "https://example.com/21",
+      "https://example.com/22",
+    ]
+    assert_equal(expected, sitemap_parser.parse)
+  end
+
+  test "sitemapindex w/ compressed" do
+    index_url = "https://example.com/sitemap-index-gz.xml"
+    urls = [
+      index_url,
+      "https://example.com/sitemap1.xml.gz",
+      "https://example.com/sitemap2.xml.gz",
+    ]
+    responses = urls.map do |url|
+      headers = {}
+      headers["Content-Encoding"] = "gzip" unless url == index_url
+      Typhoeus::Response.new(code: 200,
+                             effective_url: url,
+                             headers: headers,
+                             body: fixture_path("sitemap/#{File.basename(url)}").read)
+    end
+    urls.zip(responses) do |url, response|
+      Typhoeus.stub(url).and_return(response)
+    end
+    sitemap_parser = DaimonSkycrawlers::SitemapParser.new([index_url])
+    expected = [
+      "https://example.com/11",
+      "https://example.com/12",
+      "https://example.com/21",
+      "https://example.com/22",
+    ]
+    assert_equal(expected, sitemap_parser.parse)
+  end
 end
