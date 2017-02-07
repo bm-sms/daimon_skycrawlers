837c2113d8abc97faffcbb4eac9213a44270ba4b
Kenji Okimoto <okimoto@clear-code.com>
2017-02-07 17:48:36 +0900

    Improve test stability

--- test/daimon_skycrawlers/test_timer.rb
+++ test/daimon_skycrawlers/test_timer.rb
@@ -3,24 +3,36 @@ require "helper"
 
 class TimerTest < Test::Unit::TestCase
   test "simple timer" do
+    test_timers = Timers::Group.new
     called = false
     timers = DaimonSkycrawlers::Timer.setup_shutdown_timer("test", interval: 1) do
       called = true
     end
-    sleep(2)
-    timers.cancel
-    assert(called)
+    timers.pause
+    test_timers.after(2) do
+      assert(called)
+      timers.cancel
+    end
+    timers.resume
+    test_timers.wait
   end
 
   test "reset timer" do
+    test_timers = Timers::Group.new
     called = false
     timers = DaimonSkycrawlers::Timer.setup_shutdown_timer("test", interval: 1) do
       called = true
     end
-    sleep(0.5)
-    ActiveSupport::Notifications.publish("consume_message.songkick_queue", Time.now, Time.now, 1, { queue_name: "test" })
-    sleep(1)
-    timers.cancel
-    assert(!called)
+    timers.pause
+    test_timers.after(0.5) do
+      ActiveSupport::Notifications.publish("consume_message.songkick_queue", Time.now, Time.now, 1, { queue_name: "test" })
+    end
+    test_timers.after(2) do
+      timers.cancel
+      assert(called)
+    end
+    timers.resume
+    test_timers.wait
+    test_timers.wait
   end
 end
